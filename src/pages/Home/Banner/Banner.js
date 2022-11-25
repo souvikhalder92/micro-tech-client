90ebf424d85242cabba35b27929fc4cb7b6edaa0
souvikhalder92 <107093196+souvikhalder92@users.noreply.github.com>
2022-11-25 16:22:36 +0600

    ok

---
+++ src/pages/Home/Banner/Banner.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Banner = () => {
+    return (
+        <div className="hero min-h-screen mt-4 mb-5" style={{ backgroundImage: `url("https://i.ibb.co/tZJf9ST/ecommerce-gd81494977-1920-1.jpg")` }}>
+        <div className="hero-overlay bg-opacity-70"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Micro Tech</h1>
+            <p className="mb-5">We try to provide best product in <br/> our website.Get in touch with us.</p>
+            <Link to='/'><button className="btn btn-info">Get Started</button></Link>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Banner;
--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 
 
 const Home = () => {
     return (
         <div>
-          <p>jkkdj</p>
+          <Banner></Banner>
         </div>
     );
 };
